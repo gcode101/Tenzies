9edf2c45ba0ee12ced7a607084453df297603d93
gcode101 <gcode101@users.noreply.github.com>
2022-11-19 00:01:22 -0500

    added functionality to check for winning state

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Die from './components/Die'
 import {nanoid} from 'nanoid'
+import Confetti from 'react-confetti'
 
 function App() {
 
   const [dice, setDice] = useState(allNewDice());
+
+  const [tenzies, setTenzies] = useState(false);
+
+  useEffect(() => {
+    if (won()){
+      setTenzies(true)
+    }
+  }, [dice]);
+
+  function won() {
+    let results = true;
+    const compValue = dice[0].value;
+    dice.map(die => {
+      if (!die.isHeld || compValue !== die.value){
+        results = false;
+      }
+    });
+    return results;
+  }
 
   function allNewDice() {
     const diceObjects = [];
@@ -21,6 +41,12 @@ function App() {
     // setDice(oldDice => oldDice.map(die => {
     //     return die.isHeld ? die : {...die, value: Math.ceil(Math.random() * 6)}
     //  }))
+    console.log("inside rollDice")
+    if (tenzies) {
+      console.log("inside if tenzies")
+      setTenzies(false);
+      setDice(allNewDice());
+    }
     const resultArr = dice.map(die => {
       if (!die.isHeld){
         const randomNum = Math.ceil(Math.random() * 6);
@@ -55,6 +81,7 @@ function App() {
 
   return (
     <main className="container">
+      {tenzies && <Confetti />}
       <h1 className="title">Tenzies</h1>
       <p className="instructions">
         Roll until all dice are the same. 
@@ -63,7 +90,7 @@ function App() {
       <div className="dice">
         {diceElements}
       </div>
-      <button onClick={rollDice}>Roll</button>
+      <button onClick={rollDice}>{tenzies ? "New Game" : "Roll"}</button>
     </main>
   )
 }
